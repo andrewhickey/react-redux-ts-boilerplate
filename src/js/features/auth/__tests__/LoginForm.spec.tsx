759c2a19a88964f458f547b9f1229be4cad618fd
Andrew Hickey <andrewhickey@live.co.uk>
2017-09-05 15:56:09 +0100

    add inital tests for individual inputs

--- src/js/features/auth/__tests__/LoginForm.spec.tsx
+++ src/js/features/auth/__tests__/LoginForm.spec.tsx
@@ -1,6 +1,65 @@
+import { InputGroup } from "@blueprintjs/core"
 import { shallow } from "enzyme"
 import * as React from "react"
-import { LoginFormComponent } from "../components/LoginForm"
+import {
+  WrappedFieldInputProps,
+  WrappedFieldMetaProps,
+  WrappedFieldProps,
+} from "redux-form"
+import {
+  LoginFormComponent,
+  PasswordInput,
+  UsernameInput,
+} from "../components/LoginForm"
+
+const input: WrappedFieldInputProps = {
+  checked: false,
+  name: "username",
+  onBlur: () => {},
+  onChange: () => {},
+  onDragStart: () => {},
+  onDrop: () => {},
+  onFocus: () => {},
+  value: "",
+}
+
+const meta: WrappedFieldMetaProps = {
+  active: false,
+  asyncValidating: false,
+  autofilled: false,
+  dirty: false,
+  dispatch: () => {},
+  error: undefined,
+  form: "login",
+  initial: undefined,
+  invalid: false,
+  pristine: true,
+  submitFailed: false,
+  submitting: false,
+  touched: false,
+  valid: true,
+  visited: false,
+  warning: undefined,
+}
+
+const props: WrappedFieldProps = {
+  input,
+  meta,
+}
+
+describe("UsernameInput", () => {
+  it("Matches the snapshot", () => {
+    const wrapper = shallow(<UsernameInput {...props} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe("PasswordInput", () => {
+  it("Matches the snapshot at the top level", () => {
+    const wrapper = shallow(<PasswordInput {...props} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
 
 describe("LoginFormComponent", () => {
   it("Matches the snapshot", () => {
